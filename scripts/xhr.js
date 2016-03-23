6c260010c1eee7e13a6ed008df276b31e370bc33
omarshah5885 <umarshah0@gmail.com>
2016-03-23 18:25:21 -0400

    set an xhr request and declared object and array variables

---
+++ scripts/xhr.js
@@ -0,0 +1,31 @@
+// 
+(function() {
+
+    "use strict";
+
+	var request = new XMLHttpRequest();
+	request.open('GET', 'Comp1073/a3/json.txt', true);
+	request.send();
+	request.addEventListener('readystatechange', function() {
+	// wait for response 
+	if (request.readyState === 4) {
+	
+	// declare general object about me which will gather json data 
+	var omarInfo = {};
+	
+	// parse the json file into object
+	omarInfo = JSON.parse(request.responseText);
+	
+	// declare array representing each key from object
+	var index = [];
+	var projects = [];
+	var contact = [];
+	
+	// read the string arrays 
+	
+	// 
+	
+	}
+	} );
+
+})();
